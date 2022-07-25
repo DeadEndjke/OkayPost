93d733dd7e141f0c667056d187ccd35dbdedcc7e
unknown <perceptionarmy@icloud.com>
2022-07-26 01:02:48 +0300

    i did something

--- src/components/Content/Main/Section1/ProductPage/ProductPage.jsx
+++ src/components/Content/Main/Section1/ProductPage/ProductPage.jsx
@@ -1,28 +1,25 @@
 import s from './ProductPage.module.scss'
 import Products0 from '../../../../../state/Product0';
 import Page from './Page/Page';
-import { useEffect } from 'react';
 
 const ProductPage = ({ value }) => {
-    function renderSwitch(param) {
-        switch (param) {
-            case 0: {
-                {
-                    Products0.map((products) => (
-                        <Page key={products.id} title={products.title} />
-                    ))
-                };
-            }
-            default:
-                return 'foo';
-        }
-    }
-    useEffect(() => { renderSwitch(value) }, [])
+
 
 
     return (
         <div className={s.productPage}>
-            renderSwitch(value)
+            {(() => {
+                switch (value) {
+                    case 0:
+                        return <div>{Products0.map((products) => (
+                            <Page key={products.id} img={products.img} title={products.title} ruprice={products.ruprice} ukprice={products.ukprice} />
+                        ))}</div>;
+                    case 2:
+                        return <div>2</div>;
+                    case 3:
+                        return <div>3</div>;
+                }
+            })()}
 
         </div>
     );
